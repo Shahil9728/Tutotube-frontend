aba1dbcb6c74804fea87461bb274fbc094ee705f
Shahil Kumar <96991517+Shahil9728@users.noreply.github.com>
2023-05-04 22:44:11 +0530

    adding responsiveness

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,19 +3,33 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div id="navbar">
-            <a href="" style={{textDecoration:"none"}}>
+            <a href="" style={{ textDecoration: "none" }}>
                 <div id="logo">
                     <img src="logo.png" alt="logo" />
                     <h1>Tutotube</h1>
                 </div>
             </a>
+            <input type="checkbox" id="navbar-toggle" className="navbar-toggle" />
+            <label htmlFor="navbar-toggle" className="navbar-toggle-icons">
+                <div className="navbar-toggle-icon"></div>
+                <div className="navbar-toggle-icon"></div>
+                <div className="navbar-toggle-icon"></div>
+            </label>
+            <label htmlFor="navbar-toggle" className="navbar-toggle-label">
+                <li className="active">
+                    <a href="#">Home</a>
+                </li>
+                <li><a href="#">Pricing</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">About</a></li>
+            </label>
             <ul>
                 <li className="active">
-                    <a to="#">Home</a>
+                    <a href="#">Home</a>
                 </li>
-                <li><a to="#">Pricing</a></li>
-                <li><a to="#">Contact</a></li>
-                <li><a to="#">About</a></li>
+                <li><a href="#">Pricing</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">About</a></li>
             </ul>
             <ul>
                 <li><a href="">Sign In</a></li>
